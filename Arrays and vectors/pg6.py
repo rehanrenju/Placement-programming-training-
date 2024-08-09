1ab67934b2c7f883105c1533cd7a969eb15838e4
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:10:11 +0530

    Update pg6.py

--- Arrays and vectors/pg6.py
+++ Arrays and vectors/pg6.py
@@ -1,4 +1,19 @@
-#kjcjlksn
+
+# Given an array on integers, search a given key in the array using linear search.
+# Note: Do not use any inbuilt functions/libraries for your main logic.
+
+# Input Format
+
+# First line of input contains two integers, N - size of the array and K - search key. Second line contains the elements of the array.
+
+# Constraints
+
+# 1 <= N <= 102
+# 0 <= ar[i] <= 109
+
+# Output Format
+
+# If key is found, print the index of the array, otherwise print -1.
 def linear_search(arr, key):
     for i in range(len(arr)):
         if arr[i] == key:
