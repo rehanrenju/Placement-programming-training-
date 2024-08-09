4e9ce10adb2153d3af7b386fc6d1373b3b71b336
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:04:08 +0530

    Update pg1.py

--- Arrays and vectors/pg1.py
+++ Arrays and vectors/pg1.py
@@ -1,3 +1,19 @@
+# Find maximum element from the given array of integers.
+# Input Format
+
+# First line of input contains N - the size of the array and second line contains the elements of the array.
+
+# Constraints
+
+# 1 <= N <= 100
+# -109 <= ar[i] <= 109
+
+# Output Format
+
+# Print the maximum element of the given array.
+
+
+
 N=int(input())
 arr=list(map(int,input().split()))
 m=max(arr)
