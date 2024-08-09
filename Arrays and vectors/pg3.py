9f150b400cee6bd64f498974f2e3b409747d940d
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:05:18 +0530

    Update pg3.py

--- Arrays and vectors/pg3.py
+++ Arrays and vectors/pg3.py
@@ -1,3 +1,17 @@
+# Print sum of all odd elements in an array.
+
+# Input Format
+
+# First line of input contains N - the size of the array and second line contains array elements.
+
+# Constraints
+
+# 1 <= N <= 100
+# -109 <= ar[i] <= 109
+
+# Output Format
+
+# Print sum of all odd elements of the given array.
 N=int(input())
 arr=list(map(int,input().split()))
 odd_sum=0
