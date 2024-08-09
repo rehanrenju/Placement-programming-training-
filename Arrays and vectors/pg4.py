c0437becb797ae866f3efe502408cdec317e947c
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:07:47 +0530

    Update pg4.py

--- Arrays and vectors/pg4.py
+++ Arrays and vectors/pg4.py
@@ -1,4 +1,19 @@
-11.0 / 11.0
+# Find a duplicate element in the given array of integers. There will be only a single duplicate element in the array.
+# Note: Do not use any inbuilt functions/libraries for your main logic.
+
+# Input Format
+
+# First line of input contains size of the array - N and second line contains the elements of the array.
+
+# Constraints
+
+# 2 <= N <= 100
+# 0 <= ar[i] <= 109
+
+# Output Format
+
+# Print the duplicate element from the given array.
+
 #fdjlskn
 N=int(input())
 a=list(map(int,input().split()))
