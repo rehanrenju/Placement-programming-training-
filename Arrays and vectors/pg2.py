dd00c5b434b95fb1c74619de92c87d9fce0dcd9d
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:04:47 +0530

    Update pg2.py

--- Arrays and vectors/pg2.py
+++ Arrays and vectors/pg2.py
@@ -1,4 +1,18 @@
-#kjfcdf
+# Print array in reverse order.
+# Note: Try solving this using recursion. Do not use any inbuilt functions/libraries for your main logic.
+
+# Input Format
+
+# First line of input contains N - the size of the array and second line contains the elements of the array.
+
+# Constraints
+
+# 1 <= N <= 100
+# 0 <= ar[i] <= 1018
+
+# Output Format
+
+# Print the given array in reverse order.
 N=int(input())
 a=list(map(int,input().split()))
 def reverse_list_recursive(lst, start, end):
