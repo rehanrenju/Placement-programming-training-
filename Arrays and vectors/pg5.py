f830d52ea2cff1f5ee914b09f587a1df4d1a0fee
Rehan Renju Alunkal <94695473+rehanrenju@users.noreply.github.com>
2024-08-09 09:09:46 +0530

    Update pg5.py

--- Arrays and vectors/pg5.py
+++ Arrays and vectors/pg5.py
@@ -1,4 +1,20 @@
-#kjndlksjfv
+# Print unique elements of the array in the same order as they appear in the input.
+# Note: Do not use any inbuilt functions/libraries for your main logic.
+
+# Input Format
+
+# First line of input contains a single integer N - the size of array and second line contains array elements.
+
+# Constraints
+
+# 1 <= N <= 100
+# 0 <= ar[i] <= 109
+
+# Output Format
+
+# Print unique elements of the array.
+
+
 N = int(input())
 array = list(map(int, input().split()))
 element_counts = {}
